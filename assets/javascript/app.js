874e4b46daea3f4785671f0015fbf3841dc621c9
lisacampbell <59990057+lisaswindler@users.noreply.github.com>
2020-03-29 21:55:09 -0600

    initial files created

---
+++ assets/javascript/app.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+    //Start button on click function    
+    //Timer starts counting down
+    //at same time
+    //Question comes on screen
+    //User clicks on choice/s
+    //Only one choice allowed
+    //Click submit
+    //if correct {}
+    //congratulate player
+    //After a few seconds, display next question without user input}
+    // else if incorrect {
+    //notify
+    //display correct answer
+    //wait a few seconds, display next question
+    //}
+    //if timer reaches 0 {
+    //tell player that time is up
+    //display correct answer
+    //wait a few seconds, go to next question    
+    //}
+    //Final screen:
+    //Good job/Better luck next time
+    //Correct answers
+    //Incorrect answers:
+    //Unanswered:
+    //restart button
+});
